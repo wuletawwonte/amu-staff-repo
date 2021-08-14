7d751fb90a4bb306d7bb4ba51129d1f5abf0fb54
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-08-14 13:16:14 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Container>
+    <Button className='btn btn-danger'>What</Button>
+  </Container>  
 }
 
 export default App;
